18cb0e1b1894e14c42b4579bc667c1f322494721
hoang2807 <viethoang28072000@gmail.com>
2024-04-02 17:31:15 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const burger = document.getElementById('open')
+const overlap = document.getElementById('overlap')
+burger.addEventListener('click', function () {
+  overlap.style.width = '100%'
+})
