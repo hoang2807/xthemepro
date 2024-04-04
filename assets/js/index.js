3e5d02c41c79b847ba74bc3dfa676ffd1232dd0e
hoang2807 <viethoang28072000@gmail.com>
2024-04-04 16:57:16 +0700

    feat: add theme design

--- assets/js/index.js
+++ assets/js/index.js
@@ -82,3 +82,33 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+$(document).ready(function () {
+  $(".theme-list").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    draggable: false,
+    prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+    nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          infinite: false,
+        },
+      },
+    ],
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+  });
+});
