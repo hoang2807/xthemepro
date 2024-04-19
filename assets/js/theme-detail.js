319e583f8b9badb3a2026a50e75074baa6c4537d
hoang2807 <viethoang28072000@gmail.com>
2024-04-19 10:23:53 +0700

    feat: image modal

--- assets/js/theme-detail.js
+++ assets/js/theme-detail.js
@@ -61,23 +61,7 @@ $(document).ready(function () {
 });
 
 function showImageModal(image) {
-  // Get the modal
-  var modal = document.getElementById("myModal");
-
-  // Get the image and insert it inside the modal - use its "alt" text as a caption
-  // var img = document.getElementById("myImg");
-  var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
-
-  modal.style.display = "block";
-  modalImg.src = image.src;
-  captionText.innerHTML = image.alt;
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  }
+  const modalImage = document.getElementById('modal-image')
+  modalImage.src = image.src
+  modalImage.alt = image.alt
 }
