af567268b96c91bc10626f09d1cd4b500c25c54b
hoang2807 <viethoang28072000@gmail.com>
2024-04-19 15:37:59 +0700

    update responsive, animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,54 +5,54 @@ const li = document.querySelectorAll('.overlay-container li')
 document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger)
 
-  var tl = gsap.timeline({ paused: true });
+  // var tl = gsap.timeline({ paused: true });
 
-  function openNav() {
-    animateOpenNav();
-    burger.onclick = function (e) {
-      // Toggle reversed to it's opposite value
-      tl.reversed(!tl.reversed());
-      // Use the toggle method in the classList API
-      burger.classList.toggle("active");
-    };
-  }
+  // function openNav() {
+  //   animateOpenNav();
+  //   burger.onclick = function (e) {
+  //     // Toggle reversed to it's opposite value
+  //     tl.reversed(!tl.reversed());
+  //     // Use the toggle method in the classList API
+  //     burger.classList.toggle("active");
+  //     disableScroll()
 
-  const nav__link = document.querySelectorAll('.nav__link')
-  nav__link.forEach(element => {
-    element.addEventListener('click', function (e) {
-      tl.reversed(!tl.reversed());
-    })
-  })
+  //     if (!burger.classList.contains('active')) {
+  //       enableScroll()
+  //     }
+  //   };
+  // }
 
-  function animateOpenNav() {
-    var mobileNav = document.getElementById("mb_nav");
-    tl.to(mobileNav, {
-      duration: 0.4,
-      ease: "power3.out",
-      y: 0
-    }).to(".nav__link", {
-      opacity: 1,
-      y: 0,
-      duration: 0.3,
-      stagger: {
-        // wrap advanced options in an object
-        each: 0.2,
-        ease: "power1.in"
-      }
-    })
-      .reverse(); // Finally reverse the timeline. reversed() is true
-  }
+  // const nav__link = document.querySelectorAll('.nav__link')
+  // nav__link.forEach(element => {
+  //   element.addEventListener('click', function (e) {
+  //     tl.reversed(!tl.reversed());
+  //     enableScroll()
+  //   })
+  // })
 
-  // init
-  openNav();
+  // function animateOpenNav() {
+  //   var mobileNav = document.getElementById("mb_nav");
+  //   tl.to(mobileNav, {
+  //     duration: 0.4,
+  //     ease: "power3.out",
+  //     y: 0
+  //   }).to(".nav__link", {
+  //     opacity: 1,
+  //     y: 0,
+  //     duration: 0.3,
+  //     stagger: {
+  //       // wrap advanced options in an object
+  //       each: 0.2,
+  //       ease: "power1.in"
+  //     }
+  //   })
+  //     .reverse(); // Finally reverse the timeline. reversed() is true
+  // }
+
+  // // init
+  // openNav();
 
 });
-
-// window.addEventListener("scroll", function () {
-
-//   const maxHeight = document.body.scrollHeight - window.innerHeight;
-//   console.log((window.scrollY * 100) / maxHeight);
-// });
 
 $(document).ready(function () {
   $(".theme-list").slick({
@@ -66,13 +66,6 @@ $(document).ready(function () {
     nextArrow: $('.next'),
     dots: true,
     responsive: [
-      // {
-      //   breakpoint: 1633,
-      //   settings: {
-      //     slidesToShow: 2,
-      //     slidesToScroll: 1,
-      //   }
-      // },
       {
         breakpoint: 1120,
         settings: {
@@ -92,18 +85,7 @@ $(document).ready(function () {
           // variableWidth: false,
         }
       }
-      // {
-      //   breakpoint: 480,
-      //   settings: {
-      //     slidesToShow: 1,
-      //     slidesToScroll: 1,
-      //     arrows: false,
-      //     infinite: false,
-      //   },
-      // },
     ],
-    // autoplay: true,
-    // autoplaySpeed: 1000,
   });
 
   // reveal up
@@ -229,57 +211,20 @@ $('.btn-theme').click(function () {
             break
         }
       } else {
-        // console.log('<p>Out Of The Viewport: ' + i + '</p>')
       }
     });
   });
 })(jQuery);
 
-var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
-function preventDefault(e) {
-  e.preventDefault();
-}
 
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
+// disableScroll()
 
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; }
-  }));
-} catch (e) { }
+// $(document).ready(function () {
+//   setTimeout(function () {
+//     $('body').addClass('loaded');
+//     $('h1').css('color', '#222222');
+//     enableScroll()
+//   }, 3000);
+// });
 
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-// call this to Disable
-function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-disableScroll()
-
-$(document).ready(function () {
-  setTimeout(function () {
-    $('body').addClass('loaded');
-    $('h1').css('color', '#222222');
-    enableScroll()
-  }, 3000);
-});
---
+++ assets/js/menu.js
@@ -0,0 +1,94 @@
+const burger = document.getElementById('open')
+const overlay = document.getElementById('overlay')
+const li = document.querySelectorAll('.overlay-container li')
+
+document.addEventListener("DOMContentLoaded", function () {
+  // gsap.registerPlugin(ScrollTrigger)
+
+  var tl = gsap.timeline({ paused: true });
+
+  function openNav() {
+    animateOpenNav();
+    burger.onclick = function (e) {
+      // Toggle reversed to it's opposite value
+      tl.reversed(!tl.reversed());
+      // Use the toggle method in the classList API
+      burger.classList.toggle("active");
+      disableScroll()
+
+      if (!burger.classList.contains('active')) {
+        enableScroll()
+      }
+    };
+  }
+
+  const nav__link = document.querySelectorAll('.nav__link')
+  nav__link.forEach(element => {
+    element.addEventListener('click', function (e) {
+      tl.reversed(!tl.reversed());
+      enableScroll()
+    })
+  })
+
+  function animateOpenNav() {
+    var mobileNav = document.getElementById("mb_nav");
+    tl.to(mobileNav, {
+      duration: 0.4,
+      ease: "power3.out",
+      y: 0
+    }).to(".nav__link", {
+      opacity: 1,
+      y: 0,
+      duration: 0.3,
+      stagger: {
+        // wrap advanced options in an object
+        each: 0.2,
+        ease: "power1.in"
+      }
+    })
+      .reverse(); // Finally reverse the timeline. reversed() is true
+  }
+
+  // init
+  openNav();
+
+});
+
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; }
+  }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
